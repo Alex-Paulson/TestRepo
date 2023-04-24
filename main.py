ae65912c6f08668d1931519af8b94b60a349d9b5
Alex-Paulson <alexpaulson121@gmail.com>
2023-04-24 13:56:59 +0100

    I made changes to the main file.

--- main.py
+++ main.py
@@ -6,27 +6,59 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First Game!")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
 FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.scale(
-    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
 RED_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.scale(
-    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window():
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+def draw_window(red, yellow):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]:  # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d]:  # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w]:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s]:  # DOWN
+        yellow.y += VEL
+        
+
+def red_handle_movement(keys_pressed, red):
+        if keys_pressed[pygame.K_LEFT]:  # LEFT
+            red.x -= VEL
+        if keys_pressed[pygame.K_RIGHT]:  # RIGHT
+            red.x += VEL
+        if keys_pressed[pygame.K_UP]:  # UP
+            red.y -= VEL
+        if keys_pressed[pygame.K_DOWN]:  # DOWN
+            red.y += VEL
+
+
 def main():
+    red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -35,10 +67,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
     
     pygame.quit()
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
